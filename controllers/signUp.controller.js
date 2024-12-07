b041d4a113ded02ea20936096ffb2bd614096ecc
Chris-344 <chriscolin344@gmail.com>
2024-12-07 19:12:48 +0530

    signUp fixed

--- controllers/signUp.controller.js
+++ controllers/signUp.controller.js
@@ -11,65 +11,33 @@ export const SignUpUser = async (req, res) => {
     await connectDB();
   }
   try {
-    const { name, email, password, mobileNumber } = req.body;
-    console.log(name, email, password, mobileNumber);
+    const { name, email, password,  mobileNumber } = req.body;
+  console.log( name, email, password,  mobileNumber)
     const profileLocalPath = req.files?.profileImage[0]?.path;
-    if (!name || !email || !password || !mobileNumber) {
+    if (!name || !password || !email || !mobileNumber) {
       return res.status(400).json({ error: "All fields are required" });
     }
-    console.log(name, email, password, mobileNumber,profileLocalPath);
-
-    if (profileLocalPath) {
+    if (!profileLocalPath) {
       const profile = await uploadOnCloudinary(profileLocalPath);
       const dbRes = await User.create({
-        userName: name.toLowerCase(),
+        userName: name.toLoweCase(),
         userPassword: password,
         userEmail: email,
+      
         userMobileNumber: mobileNumber,
         userProfile: profile.url,
       });
       res.status(200).json({ message: "User registered successfully", dbRes });
     } else {
-      console.log(User);
       const dbRes = await User.create({
-        userName: name.toLowerCase(),
+        userName: name.toLoweCase(),
         userPassword: password,
-        userEmail: email.toLowerCase(),
+        userEmail: email,
         userMobileNumber: mobileNumber,
       });
-      console.log(dbRes);
-
-      
       res.status(200).json({ message: "User registered successfully", dbRes });
     }
   } catch (error) {
     res.status(400).json({ message: "Failed to sign up", error });
-  }
-};
-
-export const add_address = async (req, res) => {
-  const isConnected = mongoose.connection.readyState;
-  if (isConnected !== 1) {
-    console.log("DB is not connected, connecting again");
-    await connectDB();
-  }
-
-  try {
-    const { userId, country, state, city, streetName, landMark, pinCode, roadName } = req.body;
-    const updateResult = await User.updateOne(
-      { _id: userId },
-      {
-        addrCountry: country,
-        addrState: state,
-        addrCity: city,
-        addrStreetName: streetName,
-        addrPinCode: pinCode,
-        addrLandmark: landMark,
-        addrRoad: roadName,
-      }
-    );
-    res.status(200).json({ message: "Address added successfully", updateResult });
-  } catch (error) {
-    res.status(400).json({ message: "Failed to add address", error });
   }
 };
